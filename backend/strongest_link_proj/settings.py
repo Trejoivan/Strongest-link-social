b55cc9d29215df4f2e817cf7c76c874f9978e4e3
Ivan Trejo <trejoivan258@gmail.com>
2022-05-26 09:00:46 -0700

    launching db

--- backend/strongest_link_proj/settings.py
+++ backend/strongest_link_proj/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 
 SITE_ID = 1
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
@@ -90,6 +90,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "strongest_link_proj.urls"
@@ -160,6 +161,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 # these two settings will allow us to allow uploads from users and get access to them while in dev
+
 MEDIA_URL = "/usermedia/"
 MEDIA_ROOT = BASE_DIR / "usermedia"
 
@@ -167,3 +169,7 @@ MEDIA_ROOT = BASE_DIR / "usermedia"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+import dj_database_url 
+prod_db=dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
